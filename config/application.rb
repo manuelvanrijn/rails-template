2412f2d486800fd2c8bb62c942683baaf077982c
Manuel van Rijn <manuel@manuelvanrijn.nl>
2015-05-12 09:02:15 +0200

    set default logger format for Rails.logger

--- config/application.rb
+++ config/application.rb
@@ -8,11 +8,14 @@ gsub_file 'config/application.rb',
           '# config.i18n.default_locale = :de',
           'config.i18n.default_locale = :nl'
 
-# set supported languages
-insert_into_file 'config/application.rb', after: /^*i18n.default_locale*\n\n/ do
+# set supported languages and log formatter
+insert_into_file 'config/application.rb', after: "config.i18n.default_locale = :nl\n" do
   <<-'RUBY'
+
     # supported locales
     config.i18n.available_locales = [:nl, :en]
 
+    # use the rails log formatter for when calling Rails.logger
+    config.log_formatter = ::Logger::Formatter.new
   RUBY
 end
