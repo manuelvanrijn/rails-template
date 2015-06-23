a7199134b7def99d3661e6d6fec71b037109d9df
Manuel van Rijn <manuel@manuelvanrijn.nl>
2015-06-23 17:21:14 +0200

    shorter lib_ext require

--- config/initializers/lib_extensions.rb
+++ config/initializers/lib_extensions.rb
@@ -1,2 +1,2 @@
 # Load extensions from lib/extensions
-Dir[File.join(Rails.root, 'lib', 'extensions', '*.rb')].each { |l| require l }
+Dir[Rails.root.join('lib', 'extensions', '*.rb')].each { |l| require l }
