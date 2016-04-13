7833382b67219e38c03eb1b5ee66d82597d49f64
Manuel van Rijn <manuel@manuelvanrijn.nl>
2016-04-13 10:54:09 +0200

    uglify haml to save bandwidth

---
+++ config/initializers/haml.rb
@@ -0,0 +1,4 @@
+# Save your users bandwidth using ugly mode in HAML
+# Both Chrome and Safari automatically indent HTML for you so you dont need to
+# rely on the source for indenting to debug
+Haml::Template.options[:ugly] = true
